adecbca7f7b07ccadc9f5cdc4ba9dc0785d4d3a0
Abhishek Pandir <me19b069@smail.iitm.ac.in>
2023-02-28 17:00:52 +0530

    added dialo-gpt

---
+++ chat.py
@@ -0,0 +1,46 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+from transformers.utils import logging
+logging.set_verbosity_info()
+
+class ChatBot:
+    def __init__(self,name ="cookie", model_name = "microsoft/DialoGPT-small"):
+        self.name = name
+        self.prev_chat = []
+        self.model_name = model_name
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side='left')
+        self.model = AutoModelForCausalLM.from_pretrained(model_name)
+    def process(self, message=""):
+        input_ids = self.tokenizer.encode(message + " " + self.tokenizer.eos_token,
+                                          return_tensors="pt")
+        bot_input_ids = torch.cat([self.prev_chat, input_ids], dim=-1) if self.prev_chat.__len__() > 0 else input_ids
+            # generate a bot response
+        self.prev_chat = self.model.generate(
+            bot_input_ids,
+            max_length=1000,
+            do_sample=True,
+            top_k=100,
+            temperature=0.75,
+            pad_token_id=self.tokenizer.eos_token_id
+
+        )
+        output = self.tokenizer.decode(self.prev_chat[:, bot_input_ids.shape[-1]:][0], 
+                                       skip_special_tokens=True)
+
+        return output
+
+
+
+
+from chat import ChatBot
+if __name__ == '__main__':
+    chatbot = ChatBot(model_name = "microsoft/DialoGPT-large")
+    current_text = ""
+    while True:
+        current_text = input(">>You: ")
+        if current_text == "exit" or current_text=="bye":
+            print("Bot: I love You")
+            break
+        out_text = chatbot.process(current_text)
+        print(">>Bot: "+ out_text)
+
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from ontology_dc8f06af066e4a7880a5938933236037.simple_text import SimpleText
 from openfabric_pysdk.context import OpenfabricExecutionRay
 from openfabric_pysdk.loader import ConfigClass
 from time import time
-
+from chat import ChatBot
 
 ############################################################
 # Callback function called on update config
@@ -15,14 +15,17 @@ def config(configuration: ConfigClass):
     pass
 
 
+chatbot = ChatBot()
+
 ############################################################
 # Callback function called on each execution pass
 ############################################################
 def execute(request: SimpleText, ray: OpenfabricExecutionRay) -> SimpleText:
     output = []
+    # chatbot.prev_chat = []
     for text in request.text:
         # TODO Add code here
-        response = ''
+        response = chatbot.process(text)
         output.append(response)
 
     return SimpleText(dict(text=output))
